823652fd87ef8e49f9e6d40f647e8a7b5f73b6d9
geting-started <lokeshchand1810@gmail.com>
2025-06-11 22:54:03 +0545

    Binary search Brute Force

---
+++ Algorithms/binarysearch.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarysearch(vector<int>vec, int size, int target)
+{
+int st=0, end=size-1;
+while(st<=end)
+{
+    int mid=(st+end)/2;
+    if(target<vec[mid])
+    {
+        end=mid-1;
+    }
+    else if(target>vec[mid]){
+        st=mid+1;
+    }
+    else
+    return mid;
+}
+return -1;
+
+
+
+
+}
+
+int main()
+{
+    vector<int>vec={-1,0,3,4,5,9,12};
+    int size=vec.size();
+    int target=9;
+    cout<<binarysearch(vec,size,target);
+}
