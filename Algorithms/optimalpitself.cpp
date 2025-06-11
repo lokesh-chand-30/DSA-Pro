aeaef5f5e02bcf8a0c852d602a26f428b3d3a971
geting-started <lokeshchand1810@gmail.com>
2025-06-11 21:08:03 +0545

    Optimal product of array except itself

--- Algorithms/optimalpitself.cpp
+++ Algorithms/optimalpitself.cpp
@@ -2,9 +2,21 @@
 #include<vector>
 using namespace std;
 
-int productexceptitself()
+vector<int> productexceptitself(vector<int>vec, int n)
 {
+    vector<int> ans(n,1);
+    for(int i=1; i<n; i++)
+{
+    ans[i]=ans[i-1]*vec[i-1];
 
+}
+int suffix=1;
+for(int i=n-2; i>=0; i--)
+{
+    suffix*=vec[i+1];
+    ans[i]*=suffix;
+}
+return ans;
 
 }
 
@@ -12,4 +24,13 @@ int main()
 {
     vector<int>vec={1,2,3,4};
     int n=vec.size();
+     vector<int> result = productexceptitself(vec, n);
+
+    for (int val : result)
+    {
+        cout << val << " ";
+    }
+
+    cout << endl;
+    return 0;
 }
